f44c3b6aef086dbfa63b77a6aed7009665f14617
ZainaFat <116167431+ZainaFat@users.noreply.github.com>
2023-04-25 18:25:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function navigate1(){
+window.location = "living_room.html";
+}
+
+function navigate2(){
+    window.location = "kitchen.html";
+    }
+
+    function navigate3(){
+        window.location = "plants.html";
+        }
+
+        function navigate4(){
+            window.location = "bird_pet.html";
+            }
